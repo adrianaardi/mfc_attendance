89aa4984a4ddea824281fa8fd57876f0beabb002
adrianaardi <adrianaardi.aa@gmail.com>
2026-05-04 20:33:57 +0800

    changed to smtp

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,8 +4,6 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\URL;
-use App\Mail\BrevoTransport;
-use Illuminate\Support\Facades\Mail;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -25,9 +23,5 @@ class AppServiceProvider extends ServiceProvider
         if (app()->environment('production')) {
             \Illuminate\Support\Facades\URL::forceScheme('https');
         }
-        
-        Mail::extend('brevo', function () {
-            return new BrevoTransport(config('services.brevo.key'));
-        });
     }
 }
