08f19c77158a6dff94e277163378941e579f69bd
adrianaardi <adrianaardi.aa@gmail.com>
2026-07-15 15:11:12 +0800

    8 ppl

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -31,7 +31,7 @@ class AdminController extends Controller
             $atLeastTwoDaysQuery->where('two_days_email_status', $twoDaysEmailStatus);
         }
 
-        $atLeastTwoDays = $atLeastTwoDaysQuery->paginate(20)->withQueryString();
+        $atLeastTwoDays = $atLeastTwoDaysQuery->paginate(8)->withQueryString();
 
         $settings = [
             'registration'    => \App\Models\Setting::isEnabled('registration'),
